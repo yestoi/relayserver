9910b9d3b936d3b83a5e44c57af5915d44c2aff8
Trey U <donkywhat@gmail.com>
2014-10-09 19:52:10 -0500

    added and polished

--- web/static/js/makeboxes.js
+++ web/static/js/makeboxes.js
@@ -1,9 +1,11 @@
 makeBoxes = function(count, size_num, key) {
   var boxes = [];
+  var max = 6;
+  var min = 3;
 
   for (var i=0; i < count; i++ ) {
     var box = document.createElement('div');
-    box.className = 'box size' + size_num;
+	box.className = 'box size' + size_num; //Math.ceil( Math.random()*(max-min)+min) + Math.ceil( Math.random()*(max-min)+min );
 	box.id = key;
     // add box DOM node to array of new elements
     boxes.push( box );
