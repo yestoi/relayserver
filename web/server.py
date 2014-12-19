73ecc808f584ed11ff98ac4f6849c4c7e468ce9a
Trey U <donkywhat@gmail.com>
2014-12-19 17:15:19 -0600

    New direction

--- web/server.py
+++ web/server.py
@@ -1,9 +1,10 @@
 from gevent import monkey
 monkey.patch_all()
 
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template, copy_current_request_context
 from flask.ext.socketio import SocketIO, emit
-import time, pdb, os, argparse
+from threading import Thread
+import time, pdb, os, argparse, gevent, socket
 
 parser = argparse.ArgumentParser()
 parser.add_argument("num", help="Number of teams", type=int)
@@ -11,31 +12,38 @@ parser.add_argument("ips", help="List of ips comma seperated with 'x' in place o
 parser.add_argument("oct", help="Starting team octect", type=int)
 args = parser.parse_args()
 
-teams = {}
-for x in range(1, args.num+1):
-    teams["Team " + str(x)] = args.ips.replace("x", str(x))
-
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'r3dteam'
 socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-    return send_file('static/test/index.html')
+sess_thread = None
+main_thread = None
+relay_server = "127.0.0.1"
+relay_port = 444
+teams = []
+hackers = []
 
-@app.route('/js/<path:path>')
-def servejs(path):
-    return send_file('static/test/' + os.path.join('js', path))
+for x in range(1, args.num+1):
+    teams.append(["Team " + str(x), args.ips.replace("x", str(x)).split(",")])
 
-@app.route('/css/<path:path>')
-def servecss(path):
-    return send_file('static/test/' + os.path.join('css', path))
+def push_data():
+    s = socket.socket()
+    s.connect((relay_server, relay_port))
 
-@socketio.on('connect', namespace='/sessions')
-def sess_connect():
-    emit('team_data', teams)
+    while True:
+        s.recv(1024)
+        s.send("show relay")
+        relay = s.recv(1024)
+        s.send("show")
+        conns = s.recv(1024)
 
+        print relay
+        print conns
+
+        time.sleep(5)
+
+def push_sessions():
     while True:
         files = list(os.walk('../sessions'))[0][2]
         sessions = {}
@@ -45,8 +53,32 @@ def sess_connect():
             sessions[f] = "".join(fd.readlines()[-19:])
             fd.close()
         time.sleep(1)
+        socketio.emit('session_data', sessions, namespace='/sessions')
 
-        emit('session_data', sessions)
+@app.route('/')
+def index():
+    global sess_thread
+    if sess_thread is None:
+        sess_thread = Thread(target=push_sessions)
+        sess_thread.start()
+
+    return render_template('index.html', teams=teams, hackers=hackers)
+
+@app.route('/js/<path:path>')
+def servejs(path):
+    return send_file('static/test/' + os.path.join('js', path))
+
+@app.route('/css/<path:path>')
+def servecss(path):
+    return send_file('static/test/' + os.path.join('css', path))
+
+@socketio.on('add_hacker', namespace='/sessions')
+def add_hacker(msg):
+    hackers.append([msg['hacker'], msg['ip']])
+    emit("team_data", {hackers[-1][0]:hackers[-1][1]}, broadcast=True)
+
 
 if __name__ == '__main__':
+    main_thread = Thread(target=push_data)
+    main_thread.start()
     socketio.run(app)
