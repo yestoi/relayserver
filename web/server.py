2a981fa2c425e0f1f40a1c3804b82d14294a13ce
Trey U <donkywhat@gmail.com>
2014-12-19 08:34:44 -0600

    testing

--- web/server.py
+++ web/server.py
@@ -3,7 +3,17 @@ monkey.patch_all()
 
 from flask import Flask, send_file
 from flask.ext.socketio import SocketIO, emit
-import time, pdb, os
+import time, pdb, os, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("num", help="Number of teams", type=int)
+parser.add_argument("ips", help="List of ips comma seperated with 'x' in place of team offset")
+parser.add_argument("oct", help="Starting team octect", type=int)
+args = parser.parse_args()
+
+teams = {}
+for x in range(1, args.num+1):
+    teams["Team " + str(x)] = args.ips.replace("x", str(x))
 
 app = Flask(__name__)
 app.debug = True
@@ -12,18 +22,20 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return send_file('static/index.html')
+    return send_file('static/test/index.html')
 
 @app.route('/js/<path:path>')
 def servejs(path):
-    return send_file('static/' + os.path.join('js', path))
+    return send_file('static/test/' + os.path.join('js', path))
 
 @app.route('/css/<path:path>')
 def servecss(path):
-    return send_file('static/' + os.path.join('css', path))
+    return send_file('static/test/' + os.path.join('css', path))
 
 @socketio.on('connect', namespace='/sessions')
 def sess_connect():
+    emit('team_data', teams)
+
     while True:
         files = list(os.walk('../sessions'))[0][2]
         sessions = {}
